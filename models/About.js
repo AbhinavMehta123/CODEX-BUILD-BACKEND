a88d377218fc491b897c48eac40acfb2cb58302e
Abhinav Mehta <abhinavmehta08012005@gmail.com>
2026-02-17 22:35:51 +0530

    modify about model

--- models/About.js
+++ models/About.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
 
 const aboutSchema = new mongoose.Schema({
-  content: { type: String, required: true },
+  sectionNumber: String,
+  title: String,
+  titleAccent: String,
+  description: [String],
+  stats: [{ label: String, value: String }],
+  logs: [{ time: String, text: String, highlight: Boolean }],
+  rules: [String],
 });
 
 export default mongoose.model("About", aboutSchema);
