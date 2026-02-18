746e19cb451b8d54ce54b4844e415628e4fba73c
Abhinav Mehta <abhinavmehta08012005@gmail.com>
2026-02-18 07:47:04 +0530

    modify rule model

--- models/Rule.js
+++ models/Rule.js
@@ -1,7 +1,17 @@
 import mongoose from "mongoose";
 
 const ruleSchema = new mongoose.Schema({
-  content: { type: String, required: true },
+  sectionNumber: { type: String, required: true },
+  title: { type: String, required: true },
+  titleAccent: { type: String, required: true },
+  cards: [
+    {
+      id: { type: String, required: true },
+      title: { type: String, required: true },
+      desc: { type: String, required: true },
+      tag: { type: String, required: true },
+    },
+  ],
 });
 
 export default mongoose.model("Rule", ruleSchema);
