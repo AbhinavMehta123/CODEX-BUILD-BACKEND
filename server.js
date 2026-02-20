866d96223cc75128fbc3345f6b0c3937d3e65fb7
Abhinav Mehta <abhinavmehta08012005@gmail.com>
2026-02-20 08:21:27 +0530

    modify server file

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "codexbuildadminportal.netlify.app",
+      "https://codexbuildadminportal.netlify.app",
       "https://codexbuild.netlify.app",
       "http://localhost:3000",
       "http://localhost:5173"
